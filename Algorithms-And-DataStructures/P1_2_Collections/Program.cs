fe0b8a54d265e7917b5ee41bb8641ef688826a65
James Lind <112474995+Jlindhub@users.noreply.github.com>
2023-01-17 05:55:21 +0100

    formatting
    just makes it more pleasing to my eyes.

--- Algorithms-And-DataStructures/P1_2_Collections/Program.cs
+++ Algorithms-And-DataStructures/P1_2_Collections/Program.cs
@@ -2,22 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 
-
-List<int> intlist = new List<int>{ 137, 1000, -200 };
-
-for (int i=0; i < intlist.Count; i++)
-{
-     Console.WriteLine(intlist[i]);
-     
-}
+List<int> list = new List<int>{ 137, 1000, -200 };
+for (int i=0; i < list.Count; i++) { Console.WriteLine(list[i]); }
 
 Console.WriteLine(" "); //formatting spacer
 
 ArrayList arrayList = new ArrayList { true, "Forstbergs", 1000, .12f };
-for (int i = 0; i < arrayList.Count; i++)
-{
-     Console.WriteLine(arrayList[i]);
-}
-/* thoughts:
-it feels wrong to use for loops to utilize lists, when a foreach or a
- while loop or utilizing the pointer would be cleaner comparatively. */
+for (int i = 0; i < arrayList.Count; i++) { Console.WriteLine(arrayList[i]); }
+
+// thoughts: it feels wrong to use for loops when foreach or while loops using the pointer would be cleaner.
