eb4e03a000e9e97b683a25e3ebed6646386ee75c
James Lind <112474995+Jlindhub@users.noreply.github.com>
2023-01-18 20:05:43 +0100

    gamestatehistory try catch revision
    adds try-catch clauses to input points in order to prevent crashing due to unhandled exception.

--- Algorithms-And-DataStructures/GameStateHistory/Program.cs
+++ Algorithms-And-DataStructures/GameStateHistory/Program.cs
@@ -1,30 +1,30 @@
-﻿using System.Threading.Channels;
-using TurboCollections;
+﻿using TurboCollections;
 
 TurboLinkedStack<string> stringStack = new TurboLinkedStack<string>();
 stringStack.Push($"you are at: Main Menu  \n (1) level {stringStack.Count()+1} \n (2): exit program \n (3) settings \n (4): back (will exit program)");
-Console.WriteLine($"DEBUG COUNT: {stringStack.Count()}");
 Console.WriteLine(stringStack.Peek());
-int state = Convert.ToInt32(Console.ReadLine());
+int state;
+try { state = Convert.ToInt32(Console.ReadLine()); }
+catch (Exception) { state = 42; }
 bool isRunning = true;
 while(isRunning)
 {
     switch (state)
     {
         case 1:
-            stringStack.Push($"you are at:level {stringStack.Count()} \n your options are: \n (1)level {stringStack.Count() + 1} \n (2) main menu \n (3) settings");
+            stringStack.Push($"you are at:level {stringStack.Count()} \n your options are: \n (1)level {stringStack.Count() + 1} \n (2) main menu \n (3) settings \n (4): back");
             Console.WriteLine(stringStack.Peek());
-            Console.WriteLine($"DEBUG COUNT: {stringStack.Count()}");
-            state = Convert.ToInt32(Console.ReadLine());
+            try { state = Convert.ToInt32(Console.ReadLine()); }
+            catch (Exception) { state = 42; }
             break;
         case 2:
             if (stringStack.Count() > 1)
             {
                 stringStack.Clear();
-                stringStack.Push($"you are at: Main Menu \n (1) level {stringStack.Count() + 1} \n (2): exit program \n (3) settings");
+                stringStack.Push($"you are at: Main Menu \n (1) level {stringStack.Count() + 1} \n (2): exit program \n (3) settings \n (4): back");
                 Console.WriteLine(stringStack.Peek());
-                Console.WriteLine($"DEBUG COUNT: {stringStack.Count()}");
-                state = Convert.ToInt32(Console.ReadLine());
+                try { state = Convert.ToInt32(Console.ReadLine()); }
+                catch (Exception) { state = 42; }
                 break;
             }
             stringStack.Clear();
@@ -33,7 +33,6 @@ while(isRunning)
         case 3:
             stringStack.Push("you are at: Settings. input any key to go back.");
             Console.WriteLine(stringStack.Peek());
-            Console.WriteLine($"DEBUG COUNT: {stringStack.Count()}");
             Console.ReadKey();
             state = 2;
             break;
@@ -46,10 +45,15 @@ while(isRunning)
             }
             stringStack.Pop();
             Console.WriteLine(stringStack.Peek());
-            Console.WriteLine($"DEBUG COUNT: {stringStack.Count()}");
-            state = Convert.ToInt32(Console.ReadLine());
+            try { state = Convert.ToInt32(Console.ReadLine()); }
+            catch (Exception) { state = 42; }
+            break;
+        default:
+            Console.WriteLine("input not recognized. please try again.");
+            Console.WriteLine(stringStack.Peek());
+            try { state = Convert.ToInt32(Console.ReadLine()); }
+            catch (Exception) { state = 42; }
             break;
     }
 }
-Console.WriteLine($"DEBUG COUNT: {stringStack.Count()}");
 Console.WriteLine("exiting program...");
