6878c1336156e722b029c31773bc7db1ee3bffdc
James Lind <112474995+Jlindhub@users.noreply.github.com>
2023-01-20 14:03:53 +0100

    bug fixing and spell checking
    minor bugfixing and spell checking.

--- Algorithms-And-DataStructures/SongQueue/Program.cs
+++ Algorithms-And-DataStructures/SongQueue/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using TurboCollections;
+﻿using TurboCollections;
 
-
-
-TurboLinkedQueue<string> SongQueue = new TurboLinkedQueue<string>();
-bool isrunning = true;
-while (isrunning)
+TurboLinkedQueue<string> songQueue = new TurboLinkedQueue<string>();
+bool running = true;
+while (running)
 {
-    if(SongQueue.count>0) { Console.WriteLine($"now playing: {SongQueue.Peek()}"); }
+    if(songQueue.count>0) { Console.WriteLine($"now playing: {songQueue.Peek()}"); }
     int state;
     Console.WriteLine("what would you like to do? \n (1): add song \n (2): skip \n (3): exit program.");
     
@@ -20,16 +15,21 @@ while (isrunning)
     {
         case 1:
             Console.WriteLine("please give me the song name.");
-            SongQueue.Enqueue(Console.ReadLine());
+            string input = Console.ReadLine() ?? string.Empty;
+            if(!string.IsNullOrEmpty(input)){ songQueue.Enqueue(input); }
+            else
+            { 
+                Console.WriteLine("no input detected. returning to menu. user presumed dead. \nclearing list to preserve user dignity.");
+            }
             break;
         case 2:
-            if (SongQueue.count <=0) { Console.WriteLine("there are no songs in the queue."); }
-            else { SongQueue.Dequeue(); }
+            if (songQueue.count <=0) { Console.WriteLine("there are no songs in the queue."); }
+            else { songQueue.Dequeue(); }
             break;
         case 3:
-            SongQueue.Clear();
+            songQueue.Clear();
             Console.WriteLine("exiting program.");
-            isrunning = false;
+            running = false;
             break;
         default:
             Console.WriteLine("input not recognized. please try again.");
