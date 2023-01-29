7b7d1b5e5b29f94658d43b055815f028f52057f7
James Lind <112474995+Jlindhub@users.noreply.github.com>
2023-01-29 14:03:54 +0100

    quicksort
    this one was difficult to get working, but i managed.

--- Algorithms-And-DataStructures/TurboCollections.Test/SortingTests.cs
+++ Algorithms-And-DataStructures/TurboCollections.Test/SortingTests.cs
@@ -73,4 +73,34 @@ public class SortingTests
         for(int i=0; i<listToSort.Count; i++){ Console.Write(listToSort.Get(i) + " "); }
         //average stopwatch time for 1k list (random): 1.45sec
     }
+    [Test]
+    public void QuickSort()
+    {
+        
+        TurboLinkedList<int> listToSort = new TurboLinkedList<int> { 5, 9, 2, 4, 6, 1 };
+        TurboLinkedList<int> listTarget = new TurboLinkedList<int> { 1, 2, 4, 5, 6, 9 };
+        Stopwatch stopwatch = new Stopwatch(); 
+        stopwatch.Start(); 
+        TurboSort.Quicksort(listToSort,0,listToSort.Count-1);
+        stopwatch.Stop(); 
+        CollectionAssert.AreEqual(listTarget,listToSort);
+        Console.WriteLine(stopwatch.Elapsed);
+    }
+    [Test]
+    public void QuickSortRandomizedList()
+    {
+
+        int[] randomlist = new int[1000]; //fixed list size
+        for (int i = 0; i < randomlist.Length; i++) { randomlist[i] = _random.Next(1, 10000);} //populate list
+        TurboLinkedList<int> listToSort = new TurboLinkedList<int>();
+        listToSort.AddRange(randomlist);
+        Stopwatch stopwatch = new Stopwatch(); 
+        stopwatch.Start(); 
+        TurboSort.Quicksort(listToSort, 0, listToSort.Count-1);
+        stopwatch.Stop(); 
+        Console.WriteLine(stopwatch.Elapsed);
+        CollectionAssert.IsOrdered(listToSort);
+        for(int i=0; i<listToSort.Count; i++){ Console.Write(listToSort.Get(i) + " "); }
+        //average stopwatch time for 1k list (random): 1.9ms
+    }
 }
--- Algorithms-And-DataStructures/TurboCollections/QuickSort.cs
+++ Algorithms-And-DataStructures/TurboCollections/QuickSort.cs
@@ -3,8 +3,40 @@ namespace TurboCollections;
 
 public static partial class TurboSort
 {
+    
+    private static Random _random = new Random();
+    
     public static void Quicksort(TurboLinkedList<int> list, int low, int high)
     {
+        if (low <= high)
+        {
+            int PartitionIndex = Partition(list, low, high);
+            Quicksort(list, low, PartitionIndex-1);
+            Quicksort(list, PartitionIndex+1, high);
+        }
+    }
+
+    public static int Partition(TurboLinkedList<int> list, int low, int high)
+    {
+        int pivotValue = list.Get(high);
+        int index = low;
         
+        for (int j = index; j <= high - 1; j++)
+        {
+            if (list.Get(j) < pivotValue)
+            {
+                
+                //swap index=J and index=part.index
+                int holder = list.Get(j); //holder = j.value
+                list.Set(j,list.Get(index)); // set part.index value in j position 
+                list.Set(index,holder); // set j value in part.index
+                index++;
+            }
+        }
+        //swap high with partition index
+        int holder1 = list.Get(index); 
+        list.Set(index,list.Get(high)); // set high value in part.index position
+        list.Set(high,holder1); // set part.index value in high position
+        return index;
     }
 }
